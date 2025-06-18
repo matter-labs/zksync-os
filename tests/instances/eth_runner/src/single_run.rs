1de3a28d77a6ec0227859cf80c894e80bab5b2ac
antoniolocascio <antonio.locascio1@gmail.com>
2025-06-18 08:43:47 +0200

    Add subcommand for live running

---
+++ tests/instances/eth_runner/src/single_run.rs
@@ -0,0 +1,131 @@
+use crate::block::Block;
+use crate::block_hashes::BlockHashes;
+use crate::post_check::post_check;
+use crate::prestate::{populate_prestate, DiffTrace, PrestateTrace};
+use crate::receipts::{BlockReceipts, TransactionReceipt};
+use rig::log::info;
+use rig::*;
+use std::fs::{self, File};
+use std::io::BufReader;
+
+#[allow(clippy::too_many_arguments)]
+fn run<const RANDOMIZED: bool>(
+    mut chain: Chain<RANDOMIZED>,
+    block_context: BlockContext,
+    block_number: u64,
+    miner: alloy::primitives::Address,
+    ps_trace: PrestateTrace,
+    transactions: Vec<Vec<u8>>,
+    receipts: Vec<TransactionReceipt>,
+    diff_trace: DiffTrace,
+    block_hashes: Option<BlockHashes>,
+) -> anyhow::Result<()> {
+    chain.set_last_block_number(block_number - 1);
+
+    if let Some(block_hashes) = block_hashes {
+        chain.set_block_hashes(block_hashes.into_array(block_number))
+    }
+
+    let prestate_cache = populate_prestate(&mut chain, ps_trace);
+
+    let output = chain.run_block(transactions, Some(block_context), None);
+
+    post_check(
+        output,
+        receipts,
+        diff_trace,
+        prestate_cache,
+        ruint::aliases::B160::from_be_bytes(miner.into()),
+    );
+
+    Ok(())
+}
+
+pub fn single_run(
+    block: String,
+    calltrace: String,
+    receipts: String,
+    prestatetrace: String,
+    difftrace: String,
+    block_hashes: Option<String>,
+    randomized: bool,
+) -> anyhow::Result<()> {
+    let block = fs::read_to_string(block)?;
+    // TODO: ensure there are no calls to unsupported precompiles
+    let _calltrace = fs::read_to_string(&calltrace)?;
+    let receipts = fs::read_to_string(&receipts)?;
+    let ps_file = File::open(&prestatetrace)?;
+    let ps_reader = BufReader::new(ps_file);
+    let ps_trace: PrestateTrace = serde_json::from_reader(ps_reader)?;
+    let receipts: BlockReceipts = serde_json::from_str(&receipts).expect("valid receipts JSON");
+    let diff_file = File::open(&difftrace)?;
+    let diff_reader = BufReader::new(diff_file);
+    let diff_trace: DiffTrace = serde_json::from_reader(diff_reader)?;
+    let block_hashes: Option<BlockHashes> = block_hashes.map(|path| {
+        let hashes = fs::read_to_string(&path).expect("valid block hashes path");
+        serde_json::from_str(&hashes).expect("valid block hashes JSON")
+    });
+
+    let block: Block = serde_json::from_str(&block).expect("valid block JSON");
+    let block_number = block.result.header.number;
+    info!("Running block: {}", block_number);
+    info!("Block gas used: {}", block.result.header.gas_used);
+    // assert!(block.result.header.gas_used <= 11_000_000);
+    let miner = block.result.header.miner;
+
+    let block_context = block.get_block_context();
+    let (transactions, skipped) = block.get_transactions();
+
+    let receipts = receipts
+        .result
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, x)| if skipped.contains(&i) { None } else { Some(x) })
+        .collect();
+
+    let ps_trace = PrestateTrace {
+        result: ps_trace
+            .result
+            .into_iter()
+            .enumerate()
+            .filter_map(|(i, x)| if skipped.contains(&i) { None } else { Some(x) })
+            .collect(),
+    };
+
+    let diff_trace = DiffTrace {
+        result: diff_trace
+            .result
+            .into_iter()
+            .enumerate()
+            .filter_map(|(i, x)| if skipped.contains(&i) { None } else { Some(x) })
+            .collect(),
+    };
+
+    if randomized {
+        let chain = Chain::empty_randomized(Some(1));
+        run(
+            chain,
+            block_context,
+            block_number,
+            miner,
+            ps_trace,
+            transactions,
+            receipts,
+            diff_trace,
+            block_hashes,
+        )
+    } else {
+        let chain = Chain::empty(Some(1));
+        run(
+            chain,
+            block_context,
+            block_number,
+            miner,
+            ps_trace,
+            transactions,
+            receipts,
+            diff_trace,
+            block_hashes,
+        )
+    }
+}
