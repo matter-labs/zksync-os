71c8ed8ba348118372218e6d7f28b418fbc9b7e2
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2025-06-26 12:28:34 -0500

    chore: Update dockerfile and scripts for v0.0.3 (#105)
    ## What ❔
    
    <!-- What are the changes this PR brings about? -->
    <!-- Example: This PR adds a PR template to the repo. -->
    <!-- (For bigger PRs adding more context is appreciated) -->
    
    ## Why ❔
    
    <!-- Why are these changes done? What goal do they contribute to? What
    are the principles behind them? -->
    <!-- The `Why` has to be clear to non-Matter Labs entities running their
    own ZK Chain -->
    <!-- Example: PR templates ensure PR reviewers, observers, and future
    iterators are in context about the evolution of repos. -->
    
    ## Is this a breaking change?
    - [ ] Yes
    - [ ] No
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [ ] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [ ] Code has been formatted.
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ evm_interpreter/src/stack.rs
@@ -0,0 +1,433 @@
+// We want to avoid copies as much as we can, as it's sensitive for RISC-V 32 bit arch,
+// and make a special stack implementation.
+
+use crate::ExitCode;
+use crate::STACK_SIZE;
+use alloc::boxed::Box;
+use core::{alloc::Allocator, mem::MaybeUninit};
+use u256::U256;
+use zk_ee::system::logger::Logger;
+
+pub struct EVMStack<A: Allocator> {
+    buffer: Box<[MaybeUninit<U256>; STACK_SIZE], A>,
+    // our length both indicates how many elements are there, and
+    // at least how many of them are initialized
+    len: usize,
+}
+
+impl<A: Allocator> EVMStack<A> {
+    pub(crate) fn new_in(allocator: A) -> Self {
+        Self {
+            buffer: Box::new_in([const { MaybeUninit::uninit() }; STACK_SIZE], allocator),
+            len: 0,
+        }
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn print_stack_top(&self, logger: &mut impl Logger) {
+        unsafe {
+            if let Some(el) =
+                core::slice::from_raw_parts(self.buffer.as_ptr().cast::<U256>(), self.len).last()
+            {
+                let _ = logger.write_fmt(format_args!("Stack top = 0x{:x}\n", el));
+            } else {
+                let _ = logger.write_str("Stack top = empty\n");
+            }
+        }
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn print_stack_content(&self, logger: &mut impl Logger) {
+        unsafe {
+            let _ = logger.write_fmt(format_args!("DEPTH MAX\n"));
+            for el in core::slice::from_raw_parts(self.buffer.as_ptr().cast::<U256>(), self.len)
+                .iter()
+                .rev()
+            {
+                let _ = logger.write_fmt(format_args!("{:x}\n", el));
+            }
+            let _ = logger.write_fmt(format_args!("DEPTH 0\n"));
+        }
+    }
+
+    pub(crate) fn iter(&'_ self) -> impl Iterator<Item = &'_ U256> {
+        unsafe {
+            self.buffer[..self.len]
+                .iter()
+                .map(|el| el.assume_init_ref())
+        }
+    }
+
+    // this is kind-of overoptimization, but all push/pop ops are unrolled
+    // for ABI optimizations
+
+    #[inline(always)]
+    pub(crate) fn stack_swap(&mut self, n: usize) -> Result<(), ExitCode> {
+        let src_offset = if self.len == 0 {
+            return Err(ExitCode::StackUnderflow);
+        } else {
+            self.len - 1
+        };
+        let dst_offset = if n > src_offset {
+            return Err(ExitCode::StackUnderflow);
+        } else {
+            src_offset - n
+        };
+        unsafe {
+            // TODO: make it a method on U256
+
+            // memory swap would NOT twigger precompile, so we manually unroll
+            let src = self
+                .buffer
+                .as_mut_ptr()
+                .add(src_offset)
+                .as_mut_unchecked()
+                .assume_init_mut();
+            let dst = self
+                .buffer
+                .as_mut_ptr()
+                .add(dst_offset)
+                .as_mut_unchecked()
+                .assume_init_mut();
+            let t = src.clone();
+            Clone::clone_from(src, &*dst);
+            Clone::clone_from(dst, &t);
+
+            // core::mem::swap(
+            //     self.buffer
+            //         .as_mut_ptr()
+            //         .add(src_offset)
+            //         .as_mut_unchecked()
+            //         .assume_init_mut(),
+            //     self.buffer
+            //         .as_mut_ptr()
+            //         .add(dst_offset)
+            //         .as_mut_unchecked()
+            //         .assume_init_mut(),
+            // );
+        }
+
+        Ok(())
+    }
+
+    #[inline(always)]
+    pub(crate) fn stack_dup(&mut self, n: usize) -> Result<(), ExitCode> {
+        if self.len == STACK_SIZE {
+            return Err(ExitCode::StackOverflow);
+        }
+        let offset = if n > self.len {
+            return Err(ExitCode::StackUnderflow);
+        } else {
+            self.len - n
+        };
+        unsafe {
+            // TODO: make it a method on U256
+
+            let src_ref = self
+                .buffer
+                .as_ptr()
+                .add(offset)
+                .as_ref_unchecked()
+                .assume_init_ref();
+            let dst_ref_mut = self.buffer.as_mut_ptr().add(self.len).as_mut_unchecked();
+            U256::write_into_ptr(dst_ref_mut.as_mut_ptr(), src_ref);
+        }
+        self.len += 1;
+
+        Ok(())
+    }
+
+    #[inline(always)]
+    pub(crate) fn stack_reduce_one(&mut self) -> Result<(), ExitCode> {
+        if self.len == 0 {
+            Err(ExitCode::StackUnderflow)
+        } else {
+            self.len -= 1;
+
+            Ok(())
+        }
+    }
+
+    #[inline(always)]
+    pub fn pop_1(&'_ mut self) -> Result<&'_ U256, ExitCode> {
+        unsafe {
+            if self.len < 1 {
+                return Err(ExitCode::StackUnderflow);
+            }
+            let offset = self.len - 1;
+            let p0 = self.buffer.get_unchecked(offset).assume_init_ref();
+            self.len = offset;
+
+            Ok(p0)
+        }
+    }
+
+    #[inline(always)]
+    pub fn pop_2(&'_ mut self) -> Result<(&'_ U256, &'_ U256), ExitCode> {
+        unsafe {
+            if self.len < 2 {
+                return Err(ExitCode::StackUnderflow);
+            }
+            let mut offset = self.len - 1;
+            let p0 = self.buffer.get_unchecked(offset).assume_init_ref();
+            offset -= 1;
+            let p1 = self.buffer.get_unchecked(offset).assume_init_ref();
+            self.len = offset;
+
+            Ok((p0, p1))
+        }
+    }
+
+    #[inline(always)]
+    pub fn pop_3(&'_ mut self) -> Result<(&'_ U256, &'_ U256, &'_ U256), ExitCode> {
+        unsafe {
+            if self.len < 3 {
+                return Err(ExitCode::StackUnderflow);
+            }
+            let mut offset = self.len - 1;
+            let p0 = self.buffer.get_unchecked(offset).assume_init_ref();
+            offset -= 1;
+            let p1 = self.buffer.get_unchecked(offset).assume_init_ref();
+            offset -= 1;
+            let p2 = self.buffer.get_unchecked(offset).assume_init_ref();
+
+            self.len = offset;
+
+            Ok((p0, p1, p2))
+        }
+    }
+
+    #[inline(always)]
+    pub fn pop_4(&'_ mut self) -> Result<(&'_ U256, &'_ U256, &'_ U256, &'_ U256), ExitCode> {
+        unsafe {
+            if self.len < 4 {
+                return Err(ExitCode::StackUnderflow);
+            }
+            let mut offset = self.len - 1;
+            let p0 = self.buffer.get_unchecked(offset).assume_init_ref();
+            offset -= 1;
+            let p1 = self.buffer.get_unchecked(offset).assume_init_ref();
+            offset -= 1;
+            let p2 = self.buffer.get_unchecked(offset).assume_init_ref();
+            offset -= 1;
+            let p3 = self.buffer.get_unchecked(offset).assume_init_ref();
+
+            self.len = offset;
+
+            Ok((p0, p1, p2, p3))
+        }
+    }
+
+    #[inline(always)]
+    pub fn peek_mut(&'_ mut self) -> Result<&'_ mut U256, ExitCode> {
+        unsafe {
+            if self.len < 1 {
+                return Err(ExitCode::StackUnderflow);
+            }
+            let offset = self.len - 1;
+            let peeked = self.buffer.get_unchecked_mut(offset).assume_init_mut();
+
+            Ok(peeked)
+        }
+    }
+
+    #[inline(always)]
+    pub fn pop_1_and_peek_mut(&'_ mut self) -> Result<(&'_ U256, &'_ mut U256), ExitCode> {
+        unsafe {
+            if self.len < 2 {
+                return Err(ExitCode::StackUnderflow);
+            }
+            let mut offset = self.len - 1;
+            let p0 = self
+                .buffer
+                .as_ptr()
+                .add(offset)
+                .as_ref_unchecked()
+                .assume_init_ref();
+            self.len = offset;
+
+            offset -= 1;
+            let peeked = self
+                .buffer
+                .as_mut_ptr()
+                .add(offset)
+                .as_mut_unchecked()
+                .assume_init_mut();
+
+            Ok(((p0), peeked))
+        }
+    }
+
+    #[inline(always)]
+    pub fn pop_2_and_peek_mut(
+        &'_ mut self,
+    ) -> Result<((&'_ U256, &'_ U256), &'_ mut U256), ExitCode> {
+        unsafe {
+            if self.len < 3 {
+                return Err(ExitCode::StackUnderflow);
+            }
+            let mut offset = self.len - 1;
+            let p0 = self
+                .buffer
+                .as_ptr()
+                .add(offset)
+                .as_ref_unchecked()
+                .assume_init_ref();
+            offset -= 1;
+            let p1 = self
+                .buffer
+                .as_ptr()
+                .add(offset)
+                .as_ref_unchecked()
+                .assume_init_ref();
+            self.len = offset;
+
+            offset -= 1;
+            let peeked = self
+                .buffer
+                .as_mut_ptr()
+                .add(offset)
+                .as_mut_unchecked()
+                .assume_init_mut();
+
+            Ok(((p0, p1), peeked))
+        }
+    }
+
+    #[inline(always)]
+    pub fn pop_1_mut_and_peek(&'_ mut self) -> Result<(&'_ mut U256, &'_ mut U256), ExitCode> {
+        unsafe {
+            if self.len < 2 {
+                return Err(ExitCode::StackUnderflow);
+            }
+            let mut offset = self.len - 1;
+            let p0 = self
+                .buffer
+                .as_mut_ptr()
+                .add(offset)
+                .as_mut_unchecked()
+                .assume_init_mut();
+            self.len = offset;
+
+            offset -= 1;
+            let peeked = self
+                .buffer
+                .as_mut_ptr()
+                .add(offset)
+                .as_mut_unchecked()
+                .assume_init_mut();
+
+            Ok((p0, peeked))
+        }
+    }
+
+    #[inline(always)]
+    pub fn pop_2_mut_and_peek(
+        &'_ mut self,
+    ) -> Result<((&'_ mut U256, &'_ mut U256), &'_ mut U256), ExitCode> {
+        unsafe {
+            if self.len < 3 {
+                return Err(ExitCode::StackUnderflow);
+            }
+            let mut offset = self.len - 1;
+            let p0 = self
+                .buffer
+                .as_mut_ptr()
+                .add(offset)
+                .as_mut_unchecked()
+                .assume_init_mut();
+
+            offset -= 1;
+            let p1 = self
+                .buffer
+                .as_mut_ptr()
+                .add(offset)
+                .as_mut_unchecked()
+                .assume_init_mut();
+            self.len = offset;
+
+            offset -= 1;
+            let peeked = self
+                .buffer
+                .as_mut_ptr()
+                .add(offset)
+                .as_mut_unchecked()
+                .assume_init_mut();
+
+            Ok(((p0, p1), peeked))
+        }
+    }
+
+    #[inline(always)]
+    pub fn push_unchecked(&mut self, value: &U256) {
+        unsafe {
+            let dst_ref_mut = self.buffer.as_mut_ptr().add(self.len).as_mut_unchecked();
+            U256::write_into_ptr(dst_ref_mut.as_mut_ptr(), value);
+        }
+        self.len += 1;
+    }
+
+    #[inline(always)]
+    pub fn push_zero(&mut self) -> Result<(), ExitCode> {
+        let new_len = self.len + 1;
+        if new_len >= STACK_SIZE {
+            return Err(ExitCode::StackOverflow);
+        }
+        unsafe {
+            let dst_ref_mut = self.buffer.as_mut_ptr().add(self.len).as_mut_unchecked();
+            U256::write_zero_into_ptr(dst_ref_mut.as_mut_ptr());
+            self.len += 1;
+        }
+
+        Ok(())
+    }
+
+    #[inline(always)]
+    pub fn push_one(&mut self) -> Result<(), ExitCode> {
+        let new_len = self.len + 1;
+        if new_len >= STACK_SIZE {
+            return Err(ExitCode::StackOverflow);
+        }
+        unsafe {
+            let dst_ref_mut = self.buffer.as_mut_ptr().add(self.len).as_mut_unchecked();
+            U256::write_one_into_ptr(dst_ref_mut.as_mut_ptr());
+            self.len += 1;
+        }
+
+        Ok(())
+    }
+
+    #[inline(always)]
+    pub fn push_1(&mut self, value: &U256) -> Result<(), ExitCode> {
+        let new_len = self.len + 1;
+        if new_len >= STACK_SIZE {
+            return Err(ExitCode::StackOverflow);
+        }
+        unsafe {
+            let dst_ref_mut = self.buffer.as_mut_ptr().add(self.len).as_mut_unchecked();
+            U256::write_into_ptr(dst_ref_mut.as_mut_ptr(), value);
+            self.len += 1;
+        }
+
+        Ok(())
+    }
+
+    #[inline(always)]
+    pub fn push_2(&mut self, val0: &U256, val1: &U256) -> Result<(), ExitCode> {
+        let new_len = self.len + 2;
+        if new_len >= STACK_SIZE {
+            return Err(ExitCode::StackOverflow);
+        }
+        unsafe {
+            let dst_ref_mut = self.buffer.as_mut_ptr().add(self.len).as_mut_unchecked();
+            U256::write_into_ptr(dst_ref_mut.as_mut_ptr(), val0);
+            self.len += 1;
+
+            let dst_ref_mut = self.buffer.as_mut_ptr().add(self.len).as_mut_unchecked();
+            U256::write_into_ptr(dst_ref_mut.as_mut_ptr(), val1);
+            self.len += 1;
+        }
+
+        Ok(())
+    }
+}
