3c28110a8eceb6402d1b54e4168a417c7152e756
antoniolocascio <antonio.locascio1@gmail.com>
2025-07-08 09:57:12 +0200

    Support borrowed bitmap for preprocessing

--- evm_interpreter/src/lib.rs
+++ evm_interpreter/src/lib.rs
@@ -18,7 +18,9 @@ extern crate alloc;
 // not just some database access for storage/accounts, but also all the memory and stack.
 // Eventually we plan to try to include this abstraction back into Reth itself
 
+use core::alloc::Allocator;
 use core::ops::Range;
+use either::Either;
 
 use evm_stack::EvmStack;
 use gas::Gas;
@@ -26,7 +28,7 @@ use ruint::aliases::U256;
 use zk_ee::execution_environment_type::ExecutionEnvironmentType;
 use zk_ee::memory::slice_vec::SliceVec;
 use zk_ee::system::errors::{FatalError, InternalError, SystemError};
-use zk_ee::system::{EthereumLikeTypes, Resource, System, SystemTypes};
+use zk_ee::system::{EthereumLikeTypes, Resource, Resources, System, SystemTypes};
 
 use alloc::vec::Vec;
 use zk_ee::utils::*;
@@ -70,7 +72,7 @@ pub struct Interpreter<'a, S: EthereumLikeTypes> {
     /// Bytecode
     pub bytecode: &'a [u8],
     /// Preprocessing result
-    pub bytecode_preprocessing: BytecodePreprocessingData<S>,
+    pub bytecode_preprocessing: BytecodePreprocessingData<'a, S::Allocator>,
     /// Call value
     pub call_value: U256,
     /// Is interpreter call static.
@@ -82,49 +84,80 @@ pub struct Interpreter<'a, S: EthereumLikeTypes> {
 pub const STACK_SIZE: usize = 1024;
 pub const MAX_CODE_SIZE: usize = 0x6000;
 pub const MAX_INITCODE_SIZE: usize = MAX_CODE_SIZE * 2;
-pub const USIZE_SIZE: usize = core::mem::size_of::<usize>();
-pub const JUMPDEST_BITMAP_MAX_SIZE: usize = MAX_CODE_SIZE / USIZE_SIZE;
 pub const ERGS_PER_GAS: u64 = 256;
 pub const ERGS_PER_GAS_U256: U256 = U256::from_limbs([ERGS_PER_GAS, 0, 0, 0]);
+pub const BYTECODE_ALIGNMENT: usize = core::mem::size_of::<u64>();
 
-pub struct BytecodePreprocessingData<S: SystemTypes> {
+#[derive(Debug)]
+pub struct BytecodePreprocessingData<'a, A: Allocator> {
     pub original_bytecode_len: usize,
-    pub jumpdest_bitmap: BitMap<S>,
+    /// Either a reference to a part of the decommitted bytecode,
+    /// or an owned vec created on deployment.
+    pub jumpdest_bitmap: either::Either<BitMap<'a>, BitMapOwned<A>>,
 }
 
-impl<S: SystemTypes> BytecodePreprocessingData<S> {
-    pub(crate) fn empty(system: &mut System<S>) -> Self {
-        let jumpdest_bitmap = BitMap::<S>::empty(system);
-        let original_bytecode_len = 0;
-
+impl<'a, A: Allocator> BytecodePreprocessingData<'a, A> {
+    ///
+    /// Creates an empty bitmap, as a slice.
+    ///
+    #[inline]
+    pub fn empty() -> Self {
         Self {
-            original_bytecode_len,
-            jumpdest_bitmap,
+            original_bytecode_len: 0,
+            jumpdest_bitmap: Either::Left(BitMap::empty()),
         }
     }
 
-    pub fn is_valid_jumpdest(&self, offset: usize) -> bool {
-        if offset >= self.original_bytecode_len {
-            false
-        } else {
-            // we are in range of even the extended bytecode, so we are safe
-            unsafe { self.jumpdest_bitmap.get_bit_unchecked(offset) }
+    ///
+    /// Determine if an offset is a jumpdest.
+    ///
+    #[inline]
+    pub fn is_valid_jumpdest(&self, off: usize) -> bool {
+        match self.jumpdest_bitmap.as_ref() {
+            Either::Left(bitmap) => {
+                off < self.original_bytecode_len && unsafe { bitmap.get_bit_unchecked(off) }
+            }
+            Either::Right(bitmap) => {
+                off < self.original_bytecode_len && unsafe { bitmap.get_bit_unchecked(off) }
+            }
         }
     }
 
-    pub fn from_raw_bytecode(
-        padded_bytecode: &[u8],
-        original_len: u32,
-        system: &mut System<S>,
-        resources: &mut S::Resources,
+    ///
+    /// Parse a decommitted bytecode slice, creating a borrowed
+    /// (read-only) jumpdest bitmap.
+    ///
+    pub fn parse_bytecode(
+        bytecode: &'a [u8],
+        deployed_len: usize,
+        artifacts_len: usize,
+    ) -> (&'a [u8], Self) {
+        let padding = bytecode.len() - deployed_len - artifacts_len;
+        let (code, rest) = bytecode.split_at(deployed_len);
+        let bitmap_slice = &rest[padding..];
+
+        let preprocessing = Self {
+            original_bytecode_len: deployed_len,
+            jumpdest_bitmap: Either::Left(BitMap::from_raw(bitmap_slice)),
+        };
+        (code, preprocessing)
+    }
+
+    ///
+    /// Create an owned jumpdest-bitmap from deployed code.
+    ///
+    pub fn create_artifacts<R: Resources>(
+        allocator: A,
+        deployed_code: &[u8],
+        resources: &mut R,
     ) -> Result<Self, FatalError> {
         use crate::native_resource_constants::BYTECODE_PREPROCESSING_BYTE_NATIVE_COST;
-        use zk_ee::system::{Computational, Resources};
-        let native_cost = <S::Resources as Resources>::Native::from_computational(
-            BYTECODE_PREPROCESSING_BYTE_NATIVE_COST.saturating_mul(padded_bytecode.len() as u64),
+        use zk_ee::system::Computational;
+        let native_cost = R::Native::from_computational(
+            BYTECODE_PREPROCESSING_BYTE_NATIVE_COST.saturating_mul(deployed_code.len() as u64),
         );
         resources
-            .charge(&S::Resources::from_native(native_cost))
+            .charge(&R::from_native(native_cost))
             .map_err(|e| match e {
                 SystemError::Internal(e) => FatalError::Internal(e),
                 SystemError::OutOfErgs(_) => {
@@ -132,35 +165,60 @@ impl<S: SystemTypes> BytecodePreprocessingData<S> {
                 }
                 SystemError::OutOfNativeResources(loc) => FatalError::OutOfNativeResources(loc),
             })?;
-        let jump_map = analyze::<S>(padded_bytecode, system)
-            .map_err(|_| internal_error!("Could not preprocess bytecode"))?;
-        let new = Self {
-            original_bytecode_len: original_len as usize,
-            jumpdest_bitmap: jump_map,
-        };
-
-        Ok(new)
+        Ok(Self::create_artifacts_inner(allocator, deployed_code))
     }
-}
 
-pub struct BitMap<S: SystemTypes> {
-    inner: Vec<usize, S::Allocator>,
-}
-
-impl<S: SystemTypes> BitMap<S> {
-    pub(crate) fn empty(system: &mut System<S>) -> Self {
+    /// Useful to expose for tests.
+    pub fn create_artifacts_inner(allocator: A, deployed_code: &[u8]) -> Self {
+        let bitmap = analyze(deployed_code, allocator);
         Self {
-            inner: Vec::new_in(system.get_allocator()),
+            original_bytecode_len: deployed_code.len(),
+            jumpdest_bitmap: Either::Right(bitmap),
         }
     }
 
-    pub(crate) fn allocate_for_bit_capacity(capacity: usize, system: &mut System<S>) -> Self {
-        let usize_capacity =
-            capacity.next_multiple_of(usize::BITS as usize) / (usize::BITS as usize);
-        let mut storage = Vec::with_capacity_in(usize_capacity, system.get_allocator());
-        storage.resize(usize_capacity, 0);
+    /// usize words in the underlying bitmap.
+    #[inline(always)]
+    fn bitmap_words(&self) -> &[usize] {
+        match &self.jumpdest_bitmap {
+            Either::Left(b) => b.as_words(),
+            Either::Right(b) => b.as_words(),
+        }
+    }
+
+    ///
+    /// Returns a byte slice with the contents of the bitmap.
+    ///
+    pub fn as_slice(&self) -> &[u8] {
+        let words = self.bitmap_words();
+        let len_bytes = core::mem::size_of_val(words);
+        let ptr = words.as_ptr();
+        unsafe { core::slice::from_raw_parts(ptr.cast::<u8>(), len_bytes) }
+    }
+}
+
+///
+/// Owned version of the bitmap, represented as a usize vec.
+///
+#[derive(Debug)]
+pub struct BitMapOwned<A: Allocator> {
+    inner: Vec<usize, A>,
+}
+
+impl<A: Allocator> BitMapOwned<A> {
+    /// Allocates a bitmap for a bytecode of length [capacity].
+    pub(crate) fn allocate_for_bit_capacity(capacity: usize, allocator: A) -> Self {
+        let u64_capacity = capacity.next_multiple_of(u64::BITS as usize) / (u64::BITS as usize);
+        let word_capacity = u64_capacity * (u64::BITS as usize / usize::BITS as usize);
+        let mut storage = Vec::with_capacity_in(word_capacity, allocator);
+        storage.resize(word_capacity, 0);
 
         Self { inner: storage }
+    }
+
+    #[inline(always)]
+    pub fn as_words(&self) -> &[usize] {
+        &self.inner
     }
 
     /// # Safety
@@ -179,12 +237,12 @@ impl<S: SystemTypes> BitMap<S> {
     }
 }
 
-/// Analyzs bytecode to build a jump map.
-fn analyze<S: SystemTypes>(code: &[u8], system: &mut System<S>) -> Result<BitMap<S>, ()> {
+/// Analyzes bytecode to build a jump map.
+fn analyze<A: Allocator>(code: &[u8], allocator: A) -> BitMapOwned<A> {
     use self::opcodes as opcode;
 
     let code_len = code.len();
-    let mut jumps = BitMap::<S>::allocate_for_bit_capacity(code_len, system);
+    let mut jumps = BitMapOwned::<A>::allocate_for_bit_capacity(code_len, allocator);
 
     let mut i = 0;
     while i < code_len {
@@ -200,7 +258,45 @@ fn analyze<S: SystemTypes>(code: &[u8], system: &mut System<S>) -> Result<BitMap
         }
     }
 
-    Ok(jumps)
+    jumps
+}
+
+///
+/// Borrowed bitmap, represented as a usize slice.
+///
+#[derive(Debug)]
+pub struct BitMap<'a>(&'a [usize]);
+
+impl<'a> BitMap<'a> {
+    pub fn empty() -> Self {
+        Self(&[])
+    }
+
+    #[inline(always)]
+    pub fn as_words(&self) -> &[usize] {
+        self.0
+    }
+
+    /// View a byte-slice as a  usize-slice (no copy, no free).
+    ///
+    /// # Safety
+    /// * `slice` length is checked to be a multiple of `u64`.
+    /// * Caller guarantees the buffer lives at least `'a`.
+    pub fn from_raw(slice: &'a [u8]) -> Self {
+        assert_eq!(slice.len() % BYTECODE_ALIGNMENT, 0);
+        let words = slice.len() / core::mem::size_of::<usize>();
+        let ptr = slice.as_ptr() as *const usize;
+        let ws = unsafe { core::slice::from_raw_parts(ptr, words) };
+        Self(ws)
+    }
+
+    /// # Safety
+    /// [pos] must be within the bounds of the bitmap.
+    #[inline(always)]
+    pub unsafe fn get_bit_unchecked(&self, pos: usize) -> bool {
+        let (w, b) = (pos / usize::BITS as usize, pos % usize::BITS as usize);
+        self.0.get_unchecked(w) & (1usize << b) != 0
+    }
 }
 
 /// Result type for most instructions. Here `Err` signals that execution is suspended
