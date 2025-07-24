6c8174f219371797f413946e4fea0b6479a770b9
Igor Zhirkov <iz@matterlabs.dev>
2025-07-24 16:08:25 +0200

    localizable for runtimeerror

--- zk_ee/src/system/errors/runtime.rs
+++ zk_ee/src/system/errors/runtime.rs
@@ -1,5 +1,6 @@
 use strum_macros::IntoStaticStr;
 
+use super::location::{ErrorLocation, Localizable};
 use super::metadata::Metadata;
 
 #[cfg_attr(target_arch = "riscv32", derive(Copy))]
@@ -16,4 +17,13 @@ macro_rules! out_of_native_resources {
             $crate::location!().into(),
         )
     };
+}
+impl Localizable for RuntimeError {
+    fn get_location(&self) -> ErrorLocation {
+        match self {
+            RuntimeError::OutOfNativeResources(metadata) | RuntimeError::OutOfErgs(metadata) => {
+                metadata.location
+            }
+        }
+    }
 }
