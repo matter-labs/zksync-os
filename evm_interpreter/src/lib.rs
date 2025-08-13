6a2b787b723d5e0d05a558ce17e96b0f599b1054
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2025-08-13 16:10:19 -0500

    Fix

--- evm_interpreter/src/lib.rs
+++ evm_interpreter/src/lib.rs
@@ -167,13 +167,13 @@ impl<'ee, S: EthereumLikeTypes> EvmFrameInterface<S> for InterpreterExternal<'ee
     }
 
     #[cfg(feature = "evm_refunds")]
-    fn refund_counter(&self) -> usize {
+    fn refund_counter(&self) -> u32 {
         use zk_ee::system::IOSubsystem;
         self.system.io.get_refund_counter()
     }
 
     #[cfg(not(feature = "evm_refunds"))]
-    fn refund_counter(&self) -> usize {
+    fn refund_counter(&self) -> u32 {
         0
     }
 }
