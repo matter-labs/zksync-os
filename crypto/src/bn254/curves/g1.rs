890c2ebc169fdb4e7c0a0f71679a0dae2b405545
yoaveshel <hutctak2000@gmail.com>
2025-08-25 16:48:14 +0100

    implement allocation free scalar decomposition

--- crypto/src/bn254/curves/g1.rs
+++ crypto/src/bn254/curves/g1.rs
@@ -1,3 +1,5 @@
+use core::u64;
+
 #[cfg(any(all(target_arch = "riscv32", feature = "bigint_ops"), test))]
 use crate::ark_ff_delegation::MontFp;
 use ark_ec::{
@@ -9,7 +11,7 @@ use ark_ec::{
 };
 #[cfg(not(any(all(target_arch = "riscv32", feature = "bigint_ops"), test)))]
 use ark_ff::MontFp;
-use ark_ff::{AdditiveGroup, BigInt, Field, PrimeField, Zero};
+use ark_ff::{AdditiveGroup, BigInt, BigInteger, Field, PrimeField, Zero};
 
 use crate::bn254::fields::{Fq, Fr};
 
@@ -91,6 +93,78 @@ impl GLVConfig for Config {
         res.x *= Self::ENDO_COEFFS[0];
         res
     }
+
+    // TODO(yoaveshel):
+    //  - change to delegated U256
+    //  - using U512 everywhere is overkill
+    fn scalar_decomposition(
+        k: Self::ScalarField,
+    ) -> ((bool, Self::ScalarField), (bool, Self::ScalarField)) {
+        use ruint::aliases::{U1024, U512};
+
+        fn mul_and_shift(lhs: &U512, rhs: U512) -> U512 {
+            let x: U1024 = lhs.widening_mul(rhs);
+            U512::from_limbs(x.as_limbs().split_at(8).1.try_into().unwrap())
+        }
+
+        fn sub(lhs: U512, rhs: U512) -> (bool, U512) {
+            if lhs > rhs {
+                (true, lhs.checked_sub(rhs).unwrap())
+            } else {
+                (false, rhs.checked_sub(lhs).unwrap())
+            }
+        }
+
+        // BETA_1 = n22 * 2^512 / modulus
+        const BETA_1: U512 = U512::from_limbs([
+            7440537858994729442,
+            12177485554411886469,
+            1601953548471081566,
+            1485435879091901900,
+            6023842690951505253,
+            5534624963584316114,
+            2,
+            0,
+        ]);
+        // BETA_2 = n12 * 2^512 / modulus
+        const BETA_2: U512 = U512::from_limbs([
+            10866705332225114937,
+            3332646303595026058,
+            10351474459561409124,
+            7978627105577135858,
+            15644699364383830999,
+            2,
+            0,
+            0,
+        ]);
+
+        let s = U512::from_limbs_slice(&k.into_bigint().0);
+
+        let n11 = U512::from_limbs_slice(&Self::SCALAR_DECOMP_COEFFS[0].1 .0);
+        let n12 = U512::from_limbs_slice(&Self::SCALAR_DECOMP_COEFFS[1].1 .0);
+        let n21 = U512::from_limbs_slice(&Self::SCALAR_DECOMP_COEFFS[2].1 .0);
+        let n22 = U512::from_limbs_slice(&Self::SCALAR_DECOMP_COEFFS[3].1 .0);
+
+        let beta_1 = mul_and_shift(&BETA_1, s);
+        let beta_2 = mul_and_shift(&BETA_2, s);
+
+        let b11 = beta_1.checked_mul(n11).unwrap();
+        let b12 = beta_2.checked_mul(n21).unwrap();
+        let b1 = b11.checked_add(b12).unwrap();
+
+        let b21 = beta_1.checked_mul(n12).unwrap();
+        let b22 = beta_2.checked_mul(n22).unwrap();
+        let (b2_sign, b2) = sub(b22, b21);
+
+        let (k1_sign, k1) = sub(s, b1);
+
+        let (k2_sign, k2) = (!b2_sign, b2);
+
+        let k1 = Self::ScalarField::from_le_bytes_mod_order(&k1.to_le_bytes::<{ U512::BYTES }>());
+        let k2 = Self::ScalarField::from_le_bytes_mod_order(&k2.to_le_bytes::<{ U512::BYTES }>());
+
+        ((k1_sign, k1), (k2_sign, k2))
+    }
 }
 
 /// G1_GENERATOR_X = 1
@@ -98,3 +172,87 @@ pub const G1_GENERATOR_X: Fq = Fq::ONE;
 
 /// G1_GENERATOR_Y = 2
 pub const G1_GENERATOR_Y: Fq = MontFp!("2");
+
+#[cfg(test)]
+mod tests {
+    use super::{Config, CurveConfig, GLVConfig, PrimeField};
+    use proptest::{prop_assert_eq, proptest};
+
+    type ScalarField = <Config as CurveConfig>::ScalarField;
+
+    #[test]
+    fn compare_scalar_decomposition() {
+        proptest!(|(bytes: [u8; 32])| {
+            let k = ScalarField::from_be_bytes_mod_order(&bytes);
+
+            let (k1, k2) = <Config as GLVConfig>::scalar_decomposition(k.clone());
+            let (k1_ref, k2_ref) = scalar_decomposition_ref(k);
+
+            prop_assert_eq!(k1, k1_ref);
+            prop_assert_eq!(k2, k2_ref);
+        })
+    }
+
+    // default implementation from ark-ec
+    fn scalar_decomposition_ref(k: ScalarField) -> ((bool, ScalarField), (bool, ScalarField)) {
+        use ark_std::ops::{AddAssign, Neg};
+        use num_bigint::{BigInt, BigUint, Sign};
+        use num_integer::Integer;
+        use num_traits::{One, Signed};
+
+        let scalar: BigInt = k.into_bigint().into();
+
+        let coeff_bigints: [BigInt; 4] = Config::SCALAR_DECOMP_COEFFS.map(|x| {
+            BigInt::from_biguint(x.0.then_some(Sign::Plus).unwrap_or(Sign::Minus), x.1.into())
+        });
+
+        let [n11, n12, n21, n22] = coeff_bigints;
+
+        let r = BigInt::from(ScalarField::MODULUS);
+
+        // beta = vector([k,0]) * self.curve.N_inv
+        // The inverse of N is 1/r * Matrix([[n22, -n12], [-n21, n11]]).
+        // so β = (k*n22, -k*n12)/r
+
+        let beta_1 = {
+            let (mut div, rem) = (&scalar * &n22).div_rem(&r);
+            if (&rem + &rem) > r {
+                div.add_assign(BigInt::one());
+            }
+            div
+        };
+        let beta_2 = {
+            let (mut div, rem) = (&scalar * &n12.clone().neg()).div_rem(&r);
+            if (&rem + &rem) > r {
+                div.add_assign(BigInt::one());
+            }
+            div
+        };
+
+        // b = vector([int(beta[0]), int(beta[1])]) * self.curve.N
+        // b = (β1N11 + β2N21, β1N12 + β2N22) with the signs!
+        //   = (b11   + b12  , b21   + b22)   with the signs!
+
+        // b1
+        let b11 = &beta_1 * &n11;
+        let b12 = &beta_2 * &n21;
+        let b1 = b11 + b12;
+
+        // b2
+        let b21 = &beta_1 * &n12;
+        let b22 = &beta_2 * &n22;
+        let b2 = b21 + b22;
+
+        let k1 = &scalar - b1;
+        let k1_abs = BigUint::try_from(k1.abs()).unwrap();
+
+        // k2
+        let k2 = -b2;
+        let k2_abs = BigUint::try_from(k2.abs()).unwrap();
+
+        (
+            (k1.sign() == Sign::Plus, ScalarField::from(k1_abs)),
+            (k2.sign() == Sign::Plus, ScalarField::from(k2_abs)),
+        )
+    }
+}
