b8587c5d769a3c6700d2b7542c35d35a85875f58
yoaveshel <hutctak2000@gmail.com>
2025-08-28 15:44:17 +0100

    fmt

--- crypto/src/secp256r1/scalar/scalar64.rs
+++ crypto/src/secp256r1/scalar/scalar64.rs
@@ -39,17 +39,16 @@ impl Scalar {
 
     pub(crate) fn from_be_bytes(bytes: &[u8; 32]) -> Result<Self, Secp256r1Err> {
         let val = Self::from_be_bytes_unchecked(bytes);
-        if val.overflow()  {
-           Err(Secp256r1Err::InvalidSignature) 
+        if val.overflow() {
+            Err(Secp256r1Err::InvalidSignature)
         } else {
             Ok(val)
         }
     }
-    
+
     fn overflow(&self) -> bool {
         let (_, of) = overflowing_sub(&self.0, &MODULUS);
         of
-        
     }
 
     #[cfg(test)]
