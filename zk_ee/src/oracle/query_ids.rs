a92808ecc02b7366e2e6ce4506af66adb00c42d7
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2025-10-09 16:07:13 -0500

    Modify query ids structure

--- zk_ee/src/oracle/query_ids.rs
+++ zk_ee/src/oracle/query_ids.rs
@@ -26,7 +26,9 @@ pub const BASIC_SUBSPACE_MASK: u32 = 0x40_00_00_00;
 
 // - Second byte organizes different query subcategories
 
-/// Generic transaction and block-level queries
+/// System-level queries (e.g. disconnect oracle)
+pub const SYSTEM_SUBSPACE_MASK: u32 = BASIC_SUBSPACE_MASK | 0x00_00_00_00; // 0x40000000
+/// Generic queries
 pub const GENERIC_SUBSPACE_MASK: u32 = BASIC_SUBSPACE_MASK | 0x00_01_00_00; // 0x40010000
 /// Preimage and hash-related queries
 pub const PREIMAGE_SUBSPACE_MASK: u32 = BASIC_SUBSPACE_MASK | 0x00_02_00_00; // 0x40020000
@@ -36,27 +38,19 @@ pub const ACCOUNT_AND_STORAGE_SUBSPACE_MASK: u32 = BASIC_SUBSPACE_MASK | 0x00_03
 pub const STATE_AND_MERKLE_PATHS_SUBSPACE_MASK: u32 = BASIC_SUBSPACE_MASK | 0x00_04_00_00; // 0x40040000
 /// Computational advice queries (e.g. division/modexp advice)
 pub const ADVICE_SUBSPACE_MASK: u32 = BASIC_SUBSPACE_MASK | 0x00_05_00_00; // 0x40050000
+/// Transaction-related queries
+pub const TRANSACTION_SUBSPACE_MASK: u32 = BASIC_SUBSPACE_MASK | 0x00_06_00_00; // 0x40060000
+/// Block- (and batch-) related queries
+pub const BLOCK_SUBSPACE_MASK: u32 = BASIC_SUBSPACE_MASK | 0x00_07_00_00; // 0x40070000
 
 /// Special case: UART output query ID (for debugging purposes)
 pub const UART_QUERY_ID: u32 = 0xff_ff_ff_ff;
 
-// ========== Generic Subspace Queries ==========
-
-/// Query to get the size (in bytes) of the next transaction to be processed
-#[allow(clippy::identity_op)]
-pub const NEXT_TX_SIZE_QUERY_ID: u32 = GENERIC_SUBSPACE_MASK | 0; // 0x40010000
+// ========== Syatem Subspace Queries ==========
 
 /// Signal to disconnect from external oracle and switch to autonomous execution mode
-pub const DISCONNECT_ORACLE_QUERY_ID: u32 = GENERIC_SUBSPACE_MASK | 1; // 0x40010001
-
-/// Query to retrieve block metadata (timestamp, number, etc.) from the oracle
-pub const BLOCK_METADATA_QUERY_ID: u32 = GENERIC_SUBSPACE_MASK | 2; // 0x40010002
-
-/// Query to get transaction data words for the current transaction being processed
-pub const TX_DATA_WORDS_QUERY_ID: u32 = GENERIC_SUBSPACE_MASK | 3; // 0x40010003
-
-/// Query to get the data required for state correctness proving (e.g. previous state commitment)
-pub const ZK_PROOF_DATA_INIT_QUERY_ID: u32 = GENERIC_SUBSPACE_MASK | 4; // 0x40010004
+#[allow(clippy::identity_op)]
+pub const DISCONNECT_ORACLE_QUERY_ID: u32 = SYSTEM_SUBSPACE_MASK | 0; // 0x40000000
 
 // ========== Preimage Subspace Queries ==========
 
@@ -75,3 +69,21 @@ pub const INITIAL_STORAGE_SLOT_VALUE_QUERY_ID: u32 = ACCOUNT_AND_STORAGE_SUBSPAC
 /// Query to get the initial state commitment (root hash) before block execution
 #[allow(clippy::identity_op)]
 pub const INITIAL_STATE_COMMITMENT_QUERY_ID: u32 = STATE_AND_MERKLE_PATHS_SUBSPACE_MASK | 0; // 0x40040000
+
+// ========== Transaction Subspace Queries ==========
+
+/// Query to get the size (in bytes) of the next transaction to be processed
+#[allow(clippy::identity_op)]
+pub const NEXT_TX_SIZE_QUERY_ID: u32 = TRANSACTION_SUBSPACE_MASK | 0; // 0x40060000
+
+/// Query to get transaction data words for the current transaction being processed
+pub const TX_DATA_WORDS_QUERY_ID: u32 = TRANSACTION_SUBSPACE_MASK | 1; // 0x40060001
+
+// ========== Block or batch Subspace Queries ==========
+
+/// Query to retrieve block metadata (timestamp, number, etc.) from the oracle
+#[allow(clippy::identity_op)]
+pub const BLOCK_METADATA_QUERY_ID: u32 = BLOCK_SUBSPACE_MASK | 0; // 0x40070000
+
+/// Query to get the data required for state correctness proving (e.g. previous state commitment)
+pub const ZK_PROOF_DATA_INIT_QUERY_ID: u32 = BLOCK_SUBSPACE_MASK | 1; // 0x40070001
