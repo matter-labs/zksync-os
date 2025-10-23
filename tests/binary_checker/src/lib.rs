5ae0b872486a44488fec384d06da8007b497e283
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2025-10-23 17:24:12 -0500

    Run binary checker test for server_app

--- tests/binary_checker/src/lib.rs
+++ tests/binary_checker/src/lib.rs
@@ -13,7 +13,7 @@ mod tests {
             std::env::var("ZKSYNC_OS_DIR").unwrap_or_else(|_| String::from("../../zksync_os"));
         let file_path = PathBuf::from_str(&zksync_os_path)
             .unwrap()
-            .join("for_tests.text");
+            .join("server_app.text");
         let mut file = std::fs::File::open(file_path).unwrap();
         file.read_to_end(&mut binary).unwrap();
         assert!(binary.len() % 4 == 0);
