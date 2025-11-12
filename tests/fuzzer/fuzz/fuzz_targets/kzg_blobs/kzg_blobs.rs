a7c51a163264bbe2de614f668ad9fbef12dbc901
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2025-11-12 14:12:07 -0600

    Add a simple fuzz target

---
+++ tests/fuzzer/fuzz/fuzz_targets/kzg_blobs/kzg_blobs.rs
@@ -0,0 +1,29 @@
+#![no_main]
+
+use basic_system::system_implementation::system::da_commitment_generator::blob_commitment_generator::{ENCODABLE_BYTES_PER_BLOB, blob_versioned_hash_with_advisor, commitment_and_proof_advice::BlobCommitmentAndProofAdvisor};
+use callable_oracles::blob_kzg_commitment::blob_kzg_commitment_and_proof;
+use libfuzzer_sys::fuzz_target;
+
+struct BlobCommitmentAndProofAdvisorImplementation;
+
+impl BlobCommitmentAndProofAdvisor for BlobCommitmentAndProofAdvisorImplementation {
+    fn get_blob_commitment_and_proof_advice(
+        &mut self,
+        data: &[u8],
+    ) -> rig::basic_system::system_implementation::system::da_commitment_generator::KZGCommitmentAndProof{
+        blob_kzg_commitment_and_proof(data)
+    }
+}
+
+#[derive(Debug, Arbitrary)]
+struct Input {
+    data: [u8; ENCODABLE_BYTES_PER_BLOB],
+}
+
+fuzz_target!(|input: Input| {
+    let data = input.data;
+
+    let mut advisor = BlobCommitmentAndProofAdvisorImplementation;
+
+    let _ = blob_versioned_hash_with_advisor(&data, &mut advisor);
+});
