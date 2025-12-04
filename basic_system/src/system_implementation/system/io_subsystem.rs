b6f66ae944f552314bb46bcc4a4d3cabec4803d6
AntonD3 <antonv.dyadyuk@gmail.com>
2025-12-04 15:11:13 +0100

    Restructure code ajd add multichain root calculation tests

--- basic_system/src/system_implementation/system/io_subsystem.rs
+++ basic_system/src/system_implementation/system/io_subsystem.rs
@@ -1220,15 +1220,14 @@ impl<
         O: IOOracle,
     > FullIO<A, R, P, SF, M, O, true>
 {
+    ///
+    /// Reads multichain root from the L2MessageRoot(0x10005) contract.
+    ///
     fn read_multichain_root(&mut self) -> Bytes32 {
         const MESSAGE_ROOT_ADDRESS: B160 = B160::from_limbs([0x10005, 0, 0]);
         const SHARED_TREE_HEIGHT_STORAGE_SLOT: [u8; 32] = [
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 4,
-        ];
-        const SHARED_TREE_NODES_STORAGE_SLOT: [u8; 32] = [
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 6,
         ];
         let mut inf_resources = R::FORMAL_INFINITE;
 
@@ -1242,24 +1241,77 @@ impl<
             )
             .expect("must read MessageRoot shared tree height");
 
-        let mut hasher = crypto::sha3::Keccak256::new();
-        hasher.update(&SHARED_TREE_NODES_STORAGE_SLOT);
-        // TODO: precompute
-        let nodes_start_slot = hasher.finalize();
-        // _nodes[height]
-        let nodes_height_array_slot = U256::from_be_bytes(nodes_start_slot)
-            .add(U256::from_be_bytes(tree_height.as_u8_array()));
-        hasher = crypto::sha3::Keccak256::new();
-        hasher.update(nodes_height_array_slot.to_be_bytes::<32>());
-        // _nodes[height][0]
-        let root_slot = hasher.finalize();
+        let root_slot = calculate_multichain_root_slot(tree_height);
 
         self.storage_read::<false>(
             ExecutionEnvironmentType::NoEE,
             &mut inf_resources,
             &MESSAGE_ROOT_ADDRESS,
-            &Bytes32::from_array(root_slot),
+            &root_slot,
         )
-        .expect("must read MessageRoot shared tree height")
+            .expect("must read MessageRoot shared tree height")
+    }
+}
+
+///
+/// Calculates storage slot of multichain tree root in L2MessageRoot(0x10005) contract.
+///
+/// By convention storage slot for it should stay the same(depend only on `tree_height`).
+/// In fact, it's solidity dynamic array access `_nodes[height][0]`, which is located on slot 6.
+///
+fn calculate_multichain_root_slot(tree_height: Bytes32) -> Bytes32 {
+    // keccak256(0x0000000000000000000000000000000000000000000000000000000000000006)
+    const NODES_FIRST_ELEMENT_SLOT: [u8; 32] = [
+        0xf6, 0x52, 0x22, 0x23, 0x13, 0xe2, 0x84, 0x59, 0x52, 0x8d, 0x92, 0x0b, 0x65, 0x11, 0x5c, 0x16, 0xc0, 0x4f, 0x3e, 0xfc, 0x82, 0xaa, 0xed, 0xc9, 0x7b, 0xe5, 0x9f, 0x3f, 0x37, 0x7c, 0x0d, 0x3f
+    ];
+
+    // _nodes[height] slot
+    let nodes_height_array_slot = U256::from_be_bytes(NODES_FIRST_ELEMENT_SLOT)
+        .add(U256::from_be_bytes(tree_height.as_u8_array()));
+    let mut hasher = crypto::sha3::Keccak256::new();
+    hasher.update(nodes_height_array_slot.to_be_bytes::<32>());
+    // _nodes[height][0]
+    Bytes32::from_array(hasher.finalize())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // test cases data was made using actual solidity implementation and debug data for read tx:
+    // height 1: 0x768c3a22b1e4688c94525eb9bc2cf1ce7601fc9e871dc6e10fc44f0f06340ce1
+    // height 3: 0x38ace9b5569ba016113e31884532182bc747997e743c0b7f9c307302b5f83760
+    // height 4: 0x35817d789b7a6dbe8b95b0f21e189fb26d3d329de699cac7a267a9568298e0a5
+    #[test]
+    fn test_calculate_multichain_root_slot_tree_height_1() {
+        let tree_height = [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 1,
+        ];
+        let root_slot = calculate_multichain_root_slot(Bytes32::from_array(tree_height));
+
+        assert_eq!(root_slot.as_u8_array().to_vec(), hex::decode("768c3a22b1e4688c94525eb9bc2cf1ce7601fc9e871dc6e10fc44f0f06340ce1").unwrap());
+    }
+
+    #[test]
+    fn test_calculate_multichain_root_slot_tree_height_3() {
+        let tree_height = [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 3,
+        ];
+        let root_slot = calculate_multichain_root_slot(Bytes32::from_array(tree_height));
+
+        assert_eq!(root_slot.as_u8_array().to_vec(), hex::decode("38ace9b5569ba016113e31884532182bc747997e743c0b7f9c307302b5f83760").unwrap());
+    }
+
+    #[test]
+    fn test_calculate_multichain_root_slot_tree_height_4() {
+        let tree_height = [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 4,
+        ];
+        let root_slot = calculate_multichain_root_slot(Bytes32::from_array(tree_height));
+
+        assert_eq!(root_slot.as_u8_array().to_vec(), hex::decode("35817d789b7a6dbe8b95b0f21e189fb26d3d329de699cac7a267a9568298e0a5").unwrap());
     }
 }
