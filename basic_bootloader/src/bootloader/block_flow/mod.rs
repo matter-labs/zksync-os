c39f87f674dbdc1bce60f18b185e826bb0354549
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2025-12-16 17:23:20 -0600

    Rename zk_block_flow to zk

--- basic_bootloader/src/bootloader/block_flow/mod.rs
+++ basic_bootloader/src/bootloader/block_flow/mod.rs
@@ -7,7 +7,7 @@ mod post_tx_loop_op;
 mod pre_tx_loop_op;
 mod tx_loop;
 
-pub mod zk_block_flow;
+pub mod zk;
 
 pub use self::block_data_keeper::{BlockTransactionsDataKeeper, NopTransactionDataKeeper};
 pub use self::metadata_init_op::*;
@@ -15,4 +15,4 @@ pub use self::post_system_init_op::*;
 pub use self::post_tx_loop_op::PostTxLoopOp;
 pub use self::pre_tx_loop_op::PreTxLoopOp;
 pub use self::tx_loop::*;
-pub use self::zk_block_flow::*;
+pub use self::zk::*;
