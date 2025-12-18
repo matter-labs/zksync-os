7b7041dee64503e2054cab5d4a77d9cb38eafde3
antoniolocascio <antonio.locascio1@gmail.com>
2025-12-18 10:04:18 +0100

    Add regression test for invalid B160 coinbase

---
+++ tests/instances/unit/src/coinbase_regression.rs
@@ -0,0 +1,56 @@
+#![cfg(test)]
+
+//!
+//! Regression test for the B160 deserialization fix.
+//!
+//! This test tries to set as coinbase address a B160 with 24 bytes set instead of 20.
+//!
+
+use rig::alloy::consensus::TxEip2930;
+use rig::alloy::primitives::{address, TxKind, U256};
+use rig::ruint::aliases::B160;
+use rig::{BlockContext, Chain};
+
+#[test]
+#[should_panic]
+fn test_invalid_coinbase() {
+    let mut chain = Chain::empty(None);
+    let wallet = chain.random_signer();
+    let from = wallet.address();
+    let target_address = address!("4242000000000000000000000000000000000000");
+
+    chain.set_balance(
+        B160::from_be_bytes(from.into_array()),
+        U256::from(1_000_000_000_000_000_u64),
+    );
+
+    let tx = {
+        let tx = TxEip2930 {
+            chain_id: 37u64,
+            nonce: 0,
+            gas_price: 0,
+            gas_limit: 75_000,
+            to: TxKind::Call(target_address),
+            value: Default::default(),
+            input: Default::default(),
+            access_list: Default::default(),
+        };
+        rig::utils::sign_and_encode_alloy_tx(tx, &wallet)
+    };
+
+    // Create invalid coinbase with 24 bytes set
+    let mut coinbase = rig::ruint::aliases::B160::ZERO;
+    unsafe {
+        for limb in coinbase.as_limbs_mut() {
+            *limb = u64::MAX;
+        }
+    }
+
+    let block_context = BlockContext {
+        coinbase,
+        eip1559_basefee: U256::ZERO,
+        ..Default::default()
+    };
+
+    let _result = chain.run_block(vec![tx], Some(block_context), None, None);
+}
