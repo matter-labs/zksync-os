25eaa552abffd159e24d4f8928a300d064f383ac
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2026-05-13 15:50:38 +0300

    fix: fuzzer

--- evm_interpreter/src/u256_helpers.rs
+++ evm_interpreter/src/u256_helpers.rs
@@ -1,5 +1,7 @@
 use u256::U256;
 
+/// Returns the floor of the base-2 logarithm of the given value.
+/// Panics if the value is zero.
 pub(crate) fn log2floor(value: &U256) -> u64 {
     assert!(!value.is_zero());
     let bit_len = value.bit_len();
