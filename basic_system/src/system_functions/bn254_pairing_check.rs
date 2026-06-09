23ae3f63b470801f1beb5f03c232d603c6c4a70b
vibelyova <265594496+vibelyova@users.noreply.github.com>
2026-06-09 20:26:14 +0200

    precompiles

--- basic_system/src/system_functions/bn254_pairing_check.rs
+++ basic_system/src/system_functions/bn254_pairing_check.rs
@@ -1,7 +1,7 @@
 use super::*;
 use crate::cost_constants::{
-    BN254_PAIRING_BASE_NATIVE_COST, BN254_PAIRING_COST_PER_PAIR_ERGS,
-    BN254_PAIRING_PER_PAIR_NATIVE_COST, BN254_PAIRING_STATIC_COST_ERGS,
+    BN254_PAIRING_COST_PER_PAIR_ERGS, BN254_PAIRING_PER_PAIR_NATIVE_COST,
+    BN254_PAIRING_STATIC_COST_ERGS,
 };
 use crate::system_functions::bytereverse;
 use alloc::vec::Vec;
@@ -34,8 +34,7 @@ impl<R: Resources> SystemFunction<R, Bn254PairingCheckErrors> for Bn254PairingCh
             let num_pairs = src.len() / 192;
             let ergs_cost = BN254_PAIRING_STATIC_COST_ERGS
                 + BN254_PAIRING_COST_PER_PAIR_ERGS.times(num_pairs as u64);
-            let native_cost = (num_pairs as u64) * BN254_PAIRING_PER_PAIR_NATIVE_COST
-                + BN254_PAIRING_BASE_NATIVE_COST;
+            let native_cost = (num_pairs as u64) * BN254_PAIRING_PER_PAIR_NATIVE_COST;
 
             resources.charge(&R::from_ergs_and_native(
                 ergs_cost,
