14db3cf81c96fb15da335b76ab0d6954619374cf
Antonio Locascio <alo@matterlabs.dev>
2026-06-18 12:44:11 +0000

    fix(fri_precompile): satisfy workspace clippy

--- basic_bootloader/src/bootloader/transaction_flow/zk/validation_impl.rs
+++ basic_bootloader/src/bootloader/transaction_flow/zk/validation_impl.rs
@@ -605,6 +605,11 @@ fn maybe_drive_fri_verification<S: EthereumLikeTypes, Config: BasicBootloaderExe
 where
     S::IO: IOSubsystemExt,
 {
-    let _ = (system, transaction, verified_fri_statements);
+    let _ = (
+        system,
+        transaction,
+        verified_fri_statements,
+        core::marker::PhantomData::<Config>,
+    );
     Ok(())
 }
