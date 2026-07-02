0743758e4c897fcd2fa337d21f8929fa397bdf02
antoniolocascio <antonio.locascio1@gmail.com>
2026-07-02 12:00:59 +0200

    test(bootloader): cover blob pubdata framing

--- basic_bootloader/src/bootloader/block_flow/zk/pre_tx_loop.rs
+++ basic_bootloader/src/bootloader/block_flow/zk/pre_tx_loop.rs
@@ -10,11 +10,8 @@ use crate::bootloader::{
 };
 use zk_ee::common_structs::DACommitmentScheme;
 
-fn block_da_commitment_intrinsic_pubdata<S: EthereumLikeTypes>(system: &System<S>) -> u64
-where
-    S::IO: IOSubsystemExt,
-{
-    match system.io.da_commitment_scheme() {
+fn block_da_commitment_intrinsic_pubdata(da_commitment_scheme: Option<DACommitmentScheme>) -> u64 {
+    match da_commitment_scheme {
         Some(DACommitmentScheme::BlobsZKsyncOS) => BLOBS_ZKSYNC_OS_LENGTH_PREFIX_PUBDATA_BYTES,
         _ => 0,
     }
@@ -33,8 +30,8 @@ where
         // Create data keeper and seed block intrinsic constants
         let mut block_data = ZKBasicBlockDataKeeper::new_in(system.get_allocator());
         block_data.block_computational_native_used = BLOCK_INTRINSIC_NATIVE;
-        block_data.block_pubdata_used =
-            BLOCK_INTRINSIC_PUBDATA_BYTES + block_da_commitment_intrinsic_pubdata(system);
+        block_data.block_pubdata_used = BLOCK_INTRINSIC_PUBDATA_BYTES
+            + block_da_commitment_intrinsic_pubdata(system.io.da_commitment_scheme());
 
         // EIP-2935: store parent block hash in history storage contract
         {
@@ -43,5 +40,31 @@ where
         }
 
         Ok(block_data)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn blobs_zksync_os_prefix_is_only_charged_for_blob_scheme() {
+        assert_eq!(
+            block_da_commitment_intrinsic_pubdata(None),
+            0,
+            "missing DA scheme should not add blob framing"
+        );
+        assert_eq!(
+            block_da_commitment_intrinsic_pubdata(Some(
+                DACommitmentScheme::BlobsAndPubdataKeccak256
+            )),
+            0,
+            "keccak DA should not add the blob-only length prefix"
+        );
+        assert_eq!(
+            block_da_commitment_intrinsic_pubdata(Some(DACommitmentScheme::BlobsZKsyncOS)),
+            BLOBS_ZKSYNC_OS_LENGTH_PREFIX_PUBDATA_BYTES,
+            "blob DA must reserve the extra length field element"
+        );
     }
 }
